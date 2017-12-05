b92c7f3ca2d5b84143891f6209cf6ad889df09d9
Lauren <lauren@tomasello.me>
2017-12-05 19:21:35 +0000

    Move video source into separate class

--- src/VideoSource.js
+++ src/VideoSource.js
@@ -1,6 +1,92 @@
 export default class VideoSource {
-	constructor() {
-		this.buffer = null
-		this.bufferSize = null
+	constructor(video, canvas, buffer) {
+		// Public properties
+		this.buffer = buffer
+
+		// Private properties
+		this._video   = video
+		this._canvas  = canvas
+		this._context = null
+
+		navigator.mediaDevices
+			.getUserMedia({audio: false, video: true}) // Don't need audio
+			.then(this.handleSuccess.bind(this))
+			.catch(this.handleError.bind(this))
 	}
+
+	handleSuccess(stream) {
+		var videoTracks = stream.getVideoTracks()
+		console.log('Got stream')
+		console.log('Using video device: ' + videoTracks[0].label)
+		stream.oninactive = function() {
+			console.log('Stream inactive')
+		}
+		this._video.onplaying = () => {
+			setInterval(this.nextFrame.bind(this), 16)
+		}
+		this._video.onplay = () => console.log('video play')
+		this._video.srcObject = stream
+		this._context = this._canvas.getContext('2d')
+	}
+
+	handleError(error) {
+		if (error.name === 'ConstraintNotSatisfiedError') {
+			this.errorMsg('The resolution ' + this._video.width.exact + 'x' +
+				this._video.width.exact + ' px is not supported by your device.')
+		} else if (error.name === 'PermissionDeniedError') {
+			this.errorMsg('Permissions have not been granted to use your camera and ' +
+				'microphone, you need to allow the page access to your devices in ' +
+				'order for the demo to work.')
+		}
+		this.errorMsg('getUserMedia error: ' + error.name, error)
+	}
+
+	errorMsg(msg, error) {
+		alert(msg)
+		if (typeof error !== 'undefined') {
+			console.error(error)
+		}
+	}
+
+	nextFrame() {
+		this._canvas.width  = this._video.videoWidth/10
+		this._canvas.height = this._video.videoHeight/10
+		this._context.drawImage(this._video, 0, 0, this._canvas.width, this._canvas.height)
+		const image_data = this._context.getImageData(0, 0, this._canvas.width, this._canvas.height).data
+		let idx = 0
+		for (let i = 0; i < image_data.byteLength; i++) {
+			const r = image_data[i++]
+			const g = image_data[i++]
+			const b = image_data[i++]
+
+			const max = Math.max(r,g,b)
+			const min = Math.min(r,g,b)
+			
+			const chroma = max - min
+
+			let hue = undefined
+			if (chroma != 0) {
+				if (max == r) {
+					hue = 60 * ((g-b)/chroma % 6)
+				}
+				else if (max == g) {
+					hue = 60 * ((b-r)/chroma + 2)
+				}
+				else if (max == b) {
+					hue = 60 * ((r-g)/chroma + 4)
+				}
+				/* should do lightness and sat */
+				// scale between -1 and 1
+				this.buffer[idx++] = (hue - 180) / 360
+			}
+			else
+				this.buffer[idx++] = 0
+			// ignore alpha
+			//
+			
+			if (idx >= 3072)
+				idx -= 3072
+		}
+	}
+
 }
--- src/index.js
+++ src/index.js
@@ -1,109 +1,22 @@
+import React       from 'react'
+import {render}    from 'react-dom'
 import Sonifier    from './Sonifier.js'
 import Monitor     from './Monitor.js'
 import VideoSource from './VideoSource.js' 
 
+class UI extends React.Component {
+
+}
+
 // Don't add charts until page has loaded
 document.addEventListener('DOMContentLoaded', _ => {
-	const video  = document.querySelector('video')
-	const canvas = document.getElementById('video')
-	let context  = null
-	const constraints = {
-		audio: false,
-		video: true
-	}
-	// Put variables in global scope to make them available to the browser console.
-
-	function handleSuccess(stream) {
-		var videoTracks = stream.getVideoTracks()
-		console.log('Got stream with constraints:', constraints)
-		console.log('Using video device: ' + videoTracks[0].label)
-		stream.oninactive = function() {
-			console.log('Stream inactive')
-		}
-		window.stream   = stream // make variable available to browser console
-		video.onplaying = () => {
-			setInterval(nextFrame, 16)
-		}
-		video.onplay= () => console.log('video play')
-		video.srcObject = stream
-		context         = canvas.getContext('2d')
-	}
-
-	function handleError(error) {
-		if (error.name === 'ConstraintNotSatisfiedError') {
-			errorMsg('The resolution ' + video.width.exact + 'x' +
-				video.width.exact + ' px is not supported by your device.')
-		} else if (error.name === 'PermissionDeniedError') {
-			errorMsg('Permissions have not been granted to use your camera and ' +
-				'microphone, you need to allow the page access to your devices in ' +
-				'order for the demo to work.')
-		}
-		errorMsg('getUserMedia error: ' + error.name, error)
-	}
-
-	function errorMsg(msg, error) {
-		alert(msg)
-		if (typeof error !== 'undefined') {
-			console.error(error)
-		}
-	}
-
 	const sonifier = new Sonifier(3072)
 
-	// Audio stuff
-	//
-	/*
-	const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
-	let buffer = audioCtx.createBuffer(1, 3072, 44100);
-	let targetBuffer = new Float32Array(3072);
-	let outputBuffer = new Float32Array(3072);*/
-
-	function nextFrame() {
-		canvas.width  = video.videoWidth/10
-		canvas.height = video.videoHeight/10
-		context.drawImage(video, 0, 0, canvas.width, canvas.height)
-		const image_data = context.getImageData(0, 0, canvas.width, canvas.height).data
-		let idx = 0
-		for (let i = 0; i < image_data.byteLength; i++) {
-			const r = image_data[i++]
-			const g = image_data[i++]
-			const b = image_data[i++]
-
-			const max = Math.max(r,g,b)
-			const min = Math.min(r,g,b)
-			
-			const chroma = max - min
-
-			let hue = undefined
-			if (chroma != 0) {
-				if (max == r) {
-					hue = 60 * ((g-b)/chroma % 6)
-				}
-				else if (max == g) {
-					hue = 60 * ((b-r)/chroma + 2)
-				}
-				else if (max == b) {
-					hue = 60 * ((r-g)/chroma + 4)
-				}
-				/* should do lightness and sat */
-				// scale between -1 and 1
-				sonifier.targets.buffer[idx++] = (hue - 180) / 360
-			}
-			else
-				sonifier.targets.buffer[idx++] = 0
-			// ignore alpha
-			//
-			
-			if (idx >= 3072)
-				idx -= 3072
-		}
-	}
-
-
-	navigator.mediaDevices
-		.getUserMedia(constraints)
-		.then(handleSuccess)
-		.catch(handleError)
+	const videoSource = new VideoSource(
+		document.querySelector('video'),
+		document.querySelector('canvas#video'),
+		sonifier.targets.buffer
+	)
 
 	sonifier.start()
 
