c9cace606e198461e5293c1ced5c00a1bd16b9a8
Lauren <lauren@tomasello.me>
2018-01-09 21:47:25 +0000

    Fix broken sliders

--- src/UI.js
+++ src/UI.js
@@ -7,6 +7,12 @@ export default class UI extends React.Component {
 		super(props)
 	}
 
+	// Workaround to cause handles to render properly in React 16
+	// https://github.com/mpowaga/react-slider/issues/115
+	componentDidMount () {
+		setTimeout(() => this.forceUpdate(), 1)
+	}
+
 	render() {
 		console.log('props', this.props)
 		return <div className="ui">
@@ -14,6 +20,7 @@ export default class UI extends React.Component {
 			<HilbertOverlay order={this.props.hilbertCurveOrder} />
 			<Slider
 				className="slider -bottom"
+				defaultValue={this.props.sensitivity}
 				value={this.props.sensitivity}
 				step={1}
 				min={1}
@@ -21,6 +28,7 @@ export default class UI extends React.Component {
 				onChange={this.props.onSensitivityChange} />
 			<Slider
 				className="slider -top"
+				defaultValue={this.props.hilbertCurveOrder}
 				value={this.props.hilbertCurveOrder}
 				step={1}
 				min={1}
@@ -30,6 +38,7 @@ export default class UI extends React.Component {
 				min={1}
 				max={1000}
 				step={1}
+				defaultValue={this.props.freqRange}
 				value={this.props.freqRange}
 				onChange={this.props.onFreqRangeChange}
 				pearling
